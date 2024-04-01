ef8ceba933d4e18727852b618716aeec38922016
gihnunes <giovanna.correia.nunes@escola.pr.gov.br>
2024-04-01 12:15:04 -0300

    segundo commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-print("Programa Expreeso\m")
+print("Programa Expresso\m")
 print("1 Cadastrar Restaurante")
 print("2 Listar Restaurante")
 print("3 Ativar Restaurante")
@@ -6,6 +6,14 @@ print("4 Sair\n")
 
 input("Escolha uma opção")
 
-opcao_digitada = input
+opcao_digitada =int(input("Escolha uma opção:"))
+print("Você selecionou a opção:", opcao_digitada,"\n")
 
-print("Você escolheu:", opcao_digitada)
+if(opcao_digitada==1):
+    print("Você escolheu cadastrar um restaurante\n")
+elif(opcao_digitada==2):
+    print("Você escolheu listar os restaurantes do app\n")
+elif(opcao_digitada==3):
+    print("Você escolheu ativar um restaurante\n")
+else:
+    print("Você escolheu sair do programa\n")
